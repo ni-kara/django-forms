e6f6723bc8940b3d83b56102ea001bd0de744339
Nick <nick.karantakis@gmail.com>
2021-05-25 21:12:57 +0200

    Password encrypt

--- django_forms/forms/views.py
+++ django_forms/forms/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password
 from .forms import UserForm
+
 
 # Create your views here.
 def login(request):
@@ -10,8 +12,11 @@ def registration(request):
     form = UserForm()
     if request.method == "POST":
         form = UserForm(request.POST)
+        
         if form.is_valid():
-            form.save()
+            sign_up = form.save(commit=False)
+            sign_up.password = make_password(form.cleaned_data['password'])
+            sign_up.save()
             return redirect('login')
     context = {'form':form}
     return render(request, "registrationForm.html",context)
