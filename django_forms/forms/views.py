015fd3613edb7c1e9a7756f89e8d1c0da77003ca
Nick <nick.karantakis@gmail.com>
2021-05-31 17:53:34 +0200

    Create registration form

--- django_forms/forms/views.py
+++ django_forms/forms/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.models import auth
+from django.contrib.auth.models import auth, User
 
 
 # Create your views here.
-def login(request):
-    
+def login(request):    
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -23,6 +22,19 @@ def login(request):
     return render(request, "loginForm.html", content)
 
 def registration(request):    
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        if User.objects.get(username=username):
+            messages.info(request, 'This username exist')            
+            return redirect('/', messages= messages)
+        else:
+            user = User.objects.create(username=username, email=email, password=password)
+            user.save()        
+            redirect("/login")     
+ 
     context = {}
     return render(request, "registrationForm.html",context)
 
