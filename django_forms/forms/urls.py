6135db23cff062f8ffd7b7482e14077b701aa13c
Nick <nick.karantakis@gmail.com>
2021-05-31 16:47:36 +0200

    External database to Djnago database and login form

--- django_forms/forms/urls.py
+++ django_forms/forms/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.registration, name='registration'),
-    path('login', views.login, name='login')
+    path('login', views.login, name='login'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
--- django_forms/forms/views.py
+++ django_forms/forms/views.py
@@ -1,22 +1,31 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.hashers import make_password
-from .forms import UserForm
+from django.contrib import messages
+from django.contrib.auth.models import auth
 
 
 # Create your views here.
 def login(request):
+    
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:   
+            auth.login(request, user)             
+            return redirect('/dashboard')
+        else:
+            print("Login failed")  
+            messages.info(request, 'invalid credentials')              
+            return redirect('/login', messages=messages) 
+    
     content = {}
     return render(request, "loginForm.html", content)
 
-def registration(request):
-    form = UserForm()
-    if request.method == "POST":
-        form = UserForm(request.POST)
-        
-        if form.is_valid():
-            sign_up = form.save(commit=False)
-            sign_up.password = make_password(form.cleaned_data['password'])
-            sign_up.save()
-            return redirect('login')
-    context = {'form':form}
+def registration(request):    
+    context = {}
     return render(request, "registrationForm.html",context)
+
+def dashboard(request):
+    content = {}
+    return render(request, "dashboard.html", content)
